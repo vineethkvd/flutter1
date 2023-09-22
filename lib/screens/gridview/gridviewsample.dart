1bfb7f67c2712cafc666c4212215883638191096
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2023-09-22 16:44:21 +0530

    done

--- lib/screens/gridview/gridviewsample.dart
+++ lib/screens/gridview/gridviewsample.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class GridViewSample extends StatefulWidget {
   const GridViewSample({super.key});
 
@@ -9,6 +10,42 @@ class GridViewSample extends StatefulWidget {
 class _GridViewSampleState extends State<GridViewSample> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("GridView"),
+        centerTitle: true,
+      ),
+      // gridDelegate is a property that controls how items are shown in a list
+      body: GridView(
+          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+              crossAxisCount: 3, crossAxisSpacing: 16, mainAxisSpacing: 16),
+          scrollDirection: Axis.horizontal,
+          children: [
+            Image.network('https://picsum.photos/250?image=1'),
+            Image.network('https://picsum.photos/250?image=2'),
+            Image.network('https://picsum.photos/250?image=3'),
+            Image.network('https://picsum.photos/250?image=4'),
+            Image.network('https://picsum.photos/250?image=1'),
+            Image.network('https://picsum.photos/250?image=2'),
+            Image.network('https://picsum.photos/250?image=3'),
+            Image.network('https://picsum.photos/250?image=4'),
+            Image.network('https://picsum.photos/250?image=1'),
+            Image.network('https://picsum.photos/250?image=2'),
+            Image.network('https://picsum.photos/250?image=3'),
+            Image.network('https://picsum.photos/250?image=4'),
+            Image.network('https://picsum.photos/250?image=1'),
+            Image.network('https://picsum.photos/250?image=2'),
+            Image.network('https://picsum.photos/250?image=3'),
+            Image.network('https://picsum.photos/250?image=4'),
+            Image.network('https://picsum.photos/250?image=1'),
+            Image.network('https://picsum.photos/250?image=2'),
+            Image.network('https://picsum.photos/250?image=3'),
+            Image.network('https://picsum.photos/250?image=4'),
+            Image.network('https://picsum.photos/250?image=1'),
+            Image.network('https://picsum.photos/250?image=2'),
+            Image.network('https://picsum.photos/250?image=3'),
+            Image.network('https://picsum.photos/250?image=4'),
+          ]),
+    );
   }
 }
