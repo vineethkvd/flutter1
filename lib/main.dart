c1d267c6f186c3023620237429c1464386a7be42
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2023-09-29 16:02:11 +0530

    done

--- lib/main.dart
+++ lib/main.dart
@@ -1,75 +1,17 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
-import 'package:flutter1/sharedpreference/sharedsignin.dart';
-import 'package:flutter1/sharedpreference/sharedsignout.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+void main(){
+  runApp(const Myapp());
+}
+class Myapp extends StatefulWidget {
+  const Myapp({super.key});
 
-
-
-Future<void> main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  SharedPreferences prefs =await SharedPreferences.getInstance();
-  var email=prefs.getString("email");
-  print(email);
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: email==null?Login():Home(),));
+  @override
+  State<Myapp> createState() => _MyappState();
 }
 
-class Login extends StatelessWidget {
-  const Login({Key? key}) : super(key: key);
-
+class _MyappState extends State<Myapp> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Login"),),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            SharedPreferences pref =await SharedPreferences.getInstance();
-            pref.setString("email", "useremail@gmail.com");
-            Navigator.pushReplacement(context, MaterialPageRoute(builder: (_){
-              return Home();
-            }));
-          },
-          child: const Text("Login",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
-        ),
-      ),
-    );
-  }
-}
-
-
-
-class Home extends StatefulWidget {
-  const Home({Key? key}) : super(key: key);
-
-  @override
-  State<Home> createState() => _HomeState();
-}
-
-class _HomeState extends State<Home> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: Text("Home"),),
-      body: Column(
-        children: [
-         // Text(getData()),
-          Center(
-            child: ElevatedButton(
-              onPressed: () async {
-                SharedPreferences pref = await SharedPreferences.getInstance();
-                pref.remove("email");
-                Navigator.pushReplacement(context, MaterialPageRoute(builder: (_){
-                  return Login();
-                }));
-              },
-              child: Text("Logout",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
-            ),
-          ),
-        ],
-      ),
-    );
+    return const Placeholder();
   }
 }
---
+++ lib/sharedpreference/test.dart
@@ -0,0 +1,75 @@
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+// import 'package:flutter1/sharedpreference/sharedlogin.dart';
+// import 'package:flutter1/sharedpreference/sharedlogout.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
+//
+//
+//
+// Future<void> main() async{
+//   WidgetsFlutterBinding.ensureInitialized();
+//   SharedPreferences prefs =await SharedPreferences.getInstance();
+//   var email=prefs.getString("email");
+//   print(email);
+//   runApp(MaterialApp(
+//     debugShowCheckedModeBanner: false,
+//     home: email==null?Login():Home(),));
+// }
+//
+// class Login extends StatelessWidget {
+//   const Login({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Login"),),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () async {
+//             SharedPreferences pref =await SharedPreferences.getInstance();
+//             pref.setString("email", "useremail@gmail.com");
+//             Navigator.pushReplacement(context, MaterialPageRoute(builder: (_){
+//               return Home();
+//             }));
+//           },
+//           child: const Text("Login",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
+//         ),
+//       ),
+//     );
+//   }
+// }
+//
+//
+//
+// class Home extends StatefulWidget {
+//   const Home({Key? key}) : super(key: key);
+//
+//   @override
+//   State<Home> createState() => _HomeState();
+// }
+//
+// class _HomeState extends State<Home> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text("Home"),),
+//       body: Column(
+//         children: [
+//           // Text(getData()),
+//           Center(
+//             child: ElevatedButton(
+//               onPressed: () async {
+//                 SharedPreferences pref = await SharedPreferences.getInstance();
+//                 pref.remove("email");
+//                 Navigator.pushReplacement(context, MaterialPageRoute(builder: (_){
+//                   return Login();
+//                 }));
+//               },
+//               child: Text("Logout",style: TextStyle(fontWeight: FontWeight.bold,color: Colors.white),),
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
