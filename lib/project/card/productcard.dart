a36040917c21b3bd98c36ab976337a28e7b9e6b7
Vineeth <67091853+vineethkvd@users.noreply.github.com>
2024-01-20 04:09:47 +0530

    done

--- lib/project/card/productcard.dart
+++ lib/project/card/productcard.dart
@@ -2,6 +2,8 @@ import 'package:ecommerce_kit/src/ecommerce_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:velocity_x/velocity_x.dart';
 
+import '../../table/table_sample.dart';
+
 class EcommerceTest extends StatefulWidget {
   const EcommerceTest({super.key});
 
@@ -21,14 +23,78 @@ class _EcommerceTestState extends State<EcommerceTest> {
       body: SingleChildScrollView(
         child: Column(
           children: [
-            SingleChildScrollView(
-              scrollDirection: Axis.vertical,
-              child: Row(
-                children: [
-                  EcommerceElement.productCard(),
-                  EcommerceElement.productCard(),
-                  // EcommerceElement.productCard(),
+            Container(
+              width: MediaQuery.of(context).size.width * 0.45,
+              height: 320,
+              decoration: BoxDecoration(
+                color: Colors.white,
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 4,
+                    color: Color(0x3600000F),
+                    offset: Offset(0, 2),
+                  )
                 ],
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Padding(
+                padding: EdgeInsetsDirectional.fromSTEB(0, 0, 0, 12),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(0),
+                              bottomRight: Radius.circular(0),
+                              topLeft: Radius.circular(8),
+                              topRight: Radius.circular(8),
+                            ),
+                            child: Image.network(
+                              "https://picsum.photos/250?image=9",
+                              width: 150,
+                              height: 250,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 4, 0, 0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(8, 4, 0, 0),
+                            child: Text(
+                              "Flipkart",
+                              style: TextStyle(color: Colors.black45),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsetsDirectional.fromSTEB(0, 2, 0, 0),
+                      child: Row(
+                        mainAxisSize: MainAxisSize.max,
+                        children: [
+                          Padding(
+                            padding: EdgeInsetsDirectional.fromSTEB(8, 4, 0, 0),
+                            child: Text(
+                              '\$2000',
+                              style: TextStyle(color: Colors.black45),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
           ],
